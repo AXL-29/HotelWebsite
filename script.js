80e73e9f93b0be784f0e21ecf022716d5c5e5444
AXL-29 <gimpaojade4@gmail.com>
2025-09-16 22:02:14 +0800

    Set the font-family, size and the hover design for the navbar.

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('mobile-menu');
-const menu = document.querySelector('.menu');
-
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
